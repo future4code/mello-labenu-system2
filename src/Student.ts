e8d6a5185af7a28199c7446324465d109d908451
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-06 16:49:17 -0300

    apaguei import intruso

--- src/Student.ts
+++ src/Student.ts
@@ -1,6 +1,6 @@
 import { User } from "./User"
 import moment from "moment"
-import { MemoryMeasurement } from "vm";
+
 
 export class Student implements User {
     constructor (
@@ -13,7 +13,7 @@ export class Student implements User {
 
     public getAge(): number {
         const age: number = moment().diff(moment(this.birthDate, "DD/MM/YYYY"), "years")
-        
+
         return age
     } 
 }

e9d48ab9c2fc335410e90e38528b887dafd3ecd7
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-06 18:06:30 -0300

    class FileManager

---
+++ src/Mission.ts
@@ -0,0 +1,10 @@
+// import moment from "moment"
+
+// abstract class Mission {
+//     private name: string
+
+//     constructor (
+//         private 
+//     )
+
+// }
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import { FileManager } from "./JSONFileManager"
+
